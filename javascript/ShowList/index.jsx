2566346f5ad0a01a1f5c417277ae0b256ee6eab2
Matthew McNaney <mcnaney@gmail.com>
2017-07-20 16:07:38 -0400

    Decided against listing using react

--- javascript/ShowList/index.jsx
+++
@@ -1,7 +0,0 @@
-'use strict'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ShowList from './ShowList.jsx'
-
-ReactDOM.render(
-    <ShowList/>, document.getElementById('showlist'))

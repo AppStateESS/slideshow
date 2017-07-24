77ccb9f99a782cd0213b7aa378c967272b6a4cdc
Matthew McNaney <mcnaney@gmail.com>
2017-07-24 13:52:28 -0400

    Basics of slide form. unfinished

---
+++ javascript/SlideForm/Slide.jsx
@@ -0,0 +1,41 @@
+'use strict'
+import React, {Component} from 'react'
+import Modal from '../AddOn/Html/Modal.jsx'
+import Form from './Form.jsx'
+
+/* global $, openStatus, slide, sectionId */
+
+export default class Section extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {showForm:false}
+    this.initializeSlideLink()
+    this.showForm = this.showForm.bind(this)
+    this.hideForm = this.hideForm.bind(this)
+  }
+
+  initializeSlideLink() {
+    $('#add-slide').click(function () {
+      this.showForm()
+    }.bind(this))
+  }
+
+  slideSaved() {
+  }
+
+  showForm() {
+    this.setState({showForm: true})
+    openStatus = false
+    slide()
+  }
+
+  hideForm() {
+    this.setState({showForm: false})
+  }
+
+  render() {
+    return (
+      <div><Modal isOpen={this.state.showForm} close={this.hideForm}><Form success={this.sectionSaved}/></Modal></div>
+    )
+  }
+}
---
+++ javascript/SlideForm/index.jsx
@@ -0,0 +1,7 @@
+'use strict'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Slide from './Slide.jsx'
+
+ReactDOM.render(
+    <Slide/>, document.getElementById('slideform'))

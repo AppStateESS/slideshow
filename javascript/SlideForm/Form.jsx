a200c96c9bcf7114bc9b490ad77429ef7298138f
Matthew McNaney <mcnaney@gmail.com>
2017-07-25 13:48:30 -0400

    Added image upload code

--- javascript/SlideForm/Form.jsx
+++ javascript/SlideForm/Form.jsx
@@ -1,5 +1,5 @@
 'use strict'
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 //import PropTypes from 'prop-types'
 import InputField from '../AddOn/Form/InputField.jsx'
 import Slide from '../Resources/Slide.js'
@@ -11,23 +11,53 @@ import 'bootstrap/js/dropdown'
 import 'bootstrap/js/tooltip'
 import 'bootstrap/dist/css/bootstrap.css'
 
+/* global $, sectionId */
+
 export default class Form extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      slide: Slide
+      slide: Slide,
+      dataUri: null,
+      filename: '',
+      filetype: '',
     }
     this.onChange = this.onChange.bind(this)
+    this.imageUpload = this.imageUpload.bind(this)
+  }
+
+  imageUpload(fileList) {
+    for(let file of fileList) {
+      let formData = new FormData()
+      formData.append('sectionId', sectionId)
+      formData.append('picture', file)
+      $.ajax({
+        url: './slideshow/Slide/picture',
+        data: formData,
+        cache: false,
+        dataType: 'json',
+        type: 'post',
+        processData: false,
+        contentType: false,
+        success: function (data) {
+          ReactSummernote.insertImage(data.path, function(image){
+            image.addClass('img-responsive')
+          })
+        }.bind(this),
+        error: function (data) {}
+          .bind(this)
+      })
+    }
   }
 
   onChange(content) {
-    this.setState({content : content})
+    this.setState({ content: content })
   }
 
   render() {
     const toolbar = [
       ['style', ['style']],
-      ['font', ['bold', 'underline', 'clear']],
+      ['font', ['bold', 'italic', 'clear']],
       ['fontname', ['fontname']],
       ['para', ['ul', 'ol', 'paragraph']],
       ['table', ['table']],
@@ -39,8 +69,8 @@ export default class Form extends Component {
       dialogsInBody: true,
       toolbar: toolbar
     }
-    const {slide} = this.state
-    return (
+    const { slide } = this.state
+    return(
       <div>
         <form action="./slideshow/Slide/" method="post">
           <InputField
@@ -49,10 +79,12 @@ export default class Form extends Component {
             label="Title"
             placeholder="Title will not appear during the show"
             value={slide.title}/>
+          <label>Slide content</label>
           <ReactSummernote
             value={slide.content}
             options={options}
-            onChange={this.onChange}/>
+            onChange={this.onChange}
+            onImageUpload={this.imageUpload}/>
         </form>
       </div>
     )

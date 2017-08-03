0aa077643c1f8188997b215d5ecde70bbb1b5627
Matthew McNaney <mcnaney@gmail.com>
2017-08-03 14:32:03 -0400

    Added patch value function

--- javascript/AddOn/Mixin/Abstract.jsx
+++ javascript/AddOn/Mixin/Abstract.jsx
@@ -1,13 +1,19 @@
 'use strict'
 import React, {Component} from 'react'
 
+/* global $ */
+
 export default class Abstract extends Component {
   constructor(props) {
     super(props)
+    this.resourceName = null
     this.state = {
       resource: {},
-      errors: {}
+      errors: {},
     }
+    this.setValue = this.setValue.bind(this)
+    this.setError = this.setError.bind(this)
+    this.patchValue = this.patchValue.bind(this)
   }
 
   setValue(varname, value) {
@@ -18,6 +24,23 @@ export default class Abstract extends Component {
     let resource = this.state.resource
     resource[varname] = value
     this.setState({resource})
+  }
+
+  patchValue(varname) {
+    if (this.resourceName === null) {
+      throw 'No resourceName set'
+    }
+    $.ajax({
+      url: `./slideshow/${this.resourceName}/${this.state.resource.id}`,
+      data: {
+        varname: varname,
+        value: this.state.resource[varname],
+      },
+      dataType: 'json',
+      type: 'patch',
+      success: function () {}.bind(this),
+      error: function () {}.bind(this),
+    })
   }
 
   setError(varname, value) {

f919dfe692cc5606a54095790196d27f35a8f250
Matthew McNaney <mcnaney@gmail.com>
2017-08-03 14:32:53 -0400

    Several addition to slide form

--- javascript/SlideForm/Form.jsx
+++ javascript/SlideForm/Form.jsx
@@ -6,6 +6,7 @@ import Dropzone from 'react-dropzone'
 import ReactSummernote from 'react-summernote'
 import {options} from '../Config/Summernote.js'
 import 'react-summernote/dist/react-summernote.css'
+import './slide.css'
 
 import 'bootstrap/js/modal'
 import 'bootstrap/js/tooltip'
@@ -16,75 +17,140 @@ import 'bootstrap/dist/css/bootstrap.css'
 export default class Form extends Component {
   constructor(props) {
     super(props)
-    this.onChange = this.onChange.bind(this)
+    this.state = {
+      delayMinutes: 0,
+      delaySeconds: 0
+    }
     this.imageUpload = this.imageUpload.bind(this)
-    this.saveSlide = this.saveSlide.bind(this)
+    this.updateMinutes = this.updateMinutes.bind(this)
+    this.updateSeconds = this.updateSeconds.bind(this)
+    this.checkSeconds = this.checkSeconds.bind(this)
+    this.checkMinutes = this.checkMinutes.bind(this)
+    this.uploadBackground = this.uploadBackground.bind(this)
+    this.clearImage = this.clearImage.bind(this)
   }
 
-  uploadBackground(fileList) {}
+  updateMinutes(e) {
+    const value = e.target.value
+    this.setState({delayMinutes: value})
+  }
 
-  saveSlide() {}
+  updateSeconds(e) {
+    const value = e.target.value
+    this.setState({delaySeconds: value})
+  }
 
-  imageUpload(fileList) {
-    for (let file of fileList) {
-      let formData = new FormData()
-      formData.append('sectionId', sectionId)
-      formData.append('picture', file)
-      $.ajax({
-        url: './slideshow/Slide/picture',
-        data: formData,
-        cache: false,
-        dataType: 'json',
-        type: 'post',
-        processData: false,
-        contentType: false,
-        success: function (data) {
-          ReactSummernote.insertImage(data.path, function (image) {
-            image.addClass('img-responsive')
-          })
-        }.bind(this),
-        error: function () {}.bind(this)
+  checkMinutes() {
+    let finalMinutes = parseInt(this.state.delayMinutes)
+    if (finalMinutes > 10) {
+      finalMinutes = 10
+    }
+    this.setState({delayMinutes: finalMinutes})
+  }
+
+  clearImage(e) {
+    e.preventDefault()
+    const {slide} = this.props
+    $.ajax({
+      url: 'slideshow/Slide/' + slide.id + '/clearPicture',
+      dataType: 'json',
+      type: 'patch',
+      success: function () {
+        this.props.setValue('backgroundImage', '')
+      }.bind(this),
+      error: function () {}.bind(this),
+    })
+  }
+
+  checkSeconds() {
+    if (this.state.delaySeconds > 59) {
+      let finalMinutes = Math.floor(this.state.delaySeconds / 60)
+      let finalSeconds = parseInt(this.state.delaySeconds % 60)
+      finalMinutes = parseInt(this.state.delayMinutes) + parseInt(finalMinutes)
+      if (finalMinutes > 10) {
+        finalMinutes = 10
+        finalSeconds = 0
+      }
+      this.setState({delayMinutes: finalMinutes, delaySeconds: finalSeconds})
+    } else {
+      this.setState({
+        delaySeconds: parseInt(this.state.delaySeconds)
       })
     }
   }
 
-  onChange(content) {
-    this.setState({content: content})
+  sendImage(file) {
+    let formData = new FormData()
+    formData.append('slideId', this.props.slide.id)
+    formData.append('picture', file)
+
+    return $.ajax({
+      url: './slideshow/Slide/picture',
+      data: formData,
+      cache: false,
+      dataType: 'json',
+      type: 'post',
+      processData: false,
+      contentType: false
+    })
   }
 
-  delayMinutes() {
-    return '0'
+  uploadBackground(fileList) {
+    const response = this.sendImage(fileList[0])
+    response.success(function (data) {
+      this.props.setValue('backgroundImage', data.path)
+      this.props.patchValue('backgroundImage', data.path)
+    }.bind(this))
   }
 
-  delaySeconds() {
-    return '0'
+  imageUpload(fileList) {
+    for (let file of fileList) {
+      const response = this.sendImage(file)
+      response.success(function (data) {
+        ReactSummernote.insertImage(data.path, function (image) {
+          image.addClass('img-responsive')
+        })
+      }.bind(this))
+    }
   }
 
   render() {
     const {slide} = this.props
+    const imageStyle = {
+      maxHeight: '188px'
+    }
+    let clearButton = null
     let dropzoneImage = (
       <div>
         <i className="fa fa-camera fa-5x"></i><br/>
-        <h4>Click to browse<br/>- or -<br/>drag background image here</h4>
+        <h4>Click to browse<br/>- or -<br/>drag image here</h4>
       </div>
     )
+    if (slide.backgroundImage !== null && slide.backgroundImage.length > 0) {
+      clearButton = <button className="btn btn-warning btn-sm" onClick={this.clearImage}>Clear</button>
+      dropzoneImage = <img style={imageStyle} className="img-responsive" src={slide.backgroundImage}/>
+    }
     return (
-      <div>
+      <div className="container-fluid">
         <form action="./slideshow/Slide/" method="post">
           <InputField
             name="title"
             required={true}
-            label="Title"
+            change={this.props.setValue.bind(null, 'title')}
+            focus={true}
+            blur={this.props.patchValue.bind(null, 'title')}
             placeholder="Title will not appear during the show"
             value={slide.title}/>
           <label>Slide content</label>
           <ReactSummernote
             value={slide.content}
             options={options}
-            onChange={this.props.setValue}
+            onChange={this.props.setValue.bind(null, 'content')}
+            onBlur={this.props.patchValue.bind(null, 'content')}
             onImageUpload={this.imageUpload}/>
           <div className="row">
             <div className="col-sm-6">
+              <h3>Background image</h3>
               <Dropzone
                 ref="dropzone"
                 multiple={false}
@@ -92,16 +158,40 @@ export default class Form extends Component {
                 className="dropzone text-center pointer">
                 {dropzoneImage}
               </Dropzone>
+              {clearButton}
             </div>
             <div className="col-sm-6">
-              <label>Delay</label>
+              <label>
+                <h3>Delay</h3>
+              </label>
               <div>You may want to prevent users from moving forward until they finish
                 reading, watching a video, etc.</div>
-              <input type="text" value={this.delayMinutes()}/>:<input type="text" value={this.delaySeconds()}/>
+              <div id="delay" className="marginTop">
+                <input
+                  type="number"
+                  className="form-control"
+                  value={this.state.delayMinutes}
+                  size="2"
+                  min="0"
+                  max="10"
+                  maxLength="2"
+                  onBlur={this.checkMinutes}
+                  onChange={this.updateMinutes}/>
+                min.&nbsp;<input
+                  type="number"
+                  size="2"
+                  min="0"
+                  max="59"
+                  maxLength="2"
+                  className="form-control"
+                  onChange={this.updateSeconds}
+                  onBlur={this.checkSeconds}
+                  value={this.state.delaySeconds}/>
+                secs.
+              </div>
+              <small>Ten minute maximum</small>
             </div>
           </div>
-          <hr/>
-          <button className="btn btn-primary" onClick={this.saveSlide}>Save slide</button>
         </form>
       </div>
     )
@@ -111,5 +201,6 @@ export default class Form extends Component {
 Form.propTypes = {
   slide: PropTypes.object,
   sectionId: PropTypes.number,
-  setValue: PropTypes.func
+  setValue: PropTypes.func,
+  patchValue: PropTypes.func.isRequired,
 }

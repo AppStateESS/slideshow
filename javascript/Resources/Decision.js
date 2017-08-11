e9ad9b932da6743f9a40a4dc283758b01ec857d4
Matthew McNaney <mcnaney@gmail.com>
2017-08-11 10:22:20 -0400

    added decision class

---
+++ javascript/Resources/Decision.js
@@ -0,0 +1,10 @@
+export default class Decision {
+  constructor() {
+    this.id = 0
+    this.title = '(Untitled)'
+    this.message = ''
+    this.lockout = false
+    this.next = true
+    this.slideId = 0
+  }
+}

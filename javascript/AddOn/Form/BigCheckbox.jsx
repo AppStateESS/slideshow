6b448cb70174dea7b03ebc7837c0d163168abb13
Matthew McNaney <mcnaney@gmail.com>
2017-08-11 10:21:26 -0400

    added bigcheckbox

---
+++ javascript/AddOn/Form/BigCheckbox.jsx
@@ -0,0 +1,52 @@
+'use strict'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import empty from '../Empty.js'
+
+export default class BigCheckbox extends Component {
+  constructor(props) {
+    super(props)
+    this.handle = this.handle.bind(this)
+  }
+
+  handle()
+  {
+    this.props.handle(empty(this.props.checked))
+  }
+
+  render() {
+    const mute = {
+      color: '#666'
+    }
+    const point = {
+      cursor: 'pointer',
+      display: 'inline-block'
+    }
+    const labelText = {
+      fontSize : '20px',
+      display: 'inline-block',
+      marginTop: '4px'
+    }
+
+    return (
+      <div onClick={this.handle} style={point} className="big-checkbox">
+        <div className="fa-stack fa-lg pull-left">
+          <i className="fa fa-square-o fa-stack-2x" style={mute}></i>
+          {empty(this.props.checked) ? null :
+          <i className="fa fa-check text-success fa-stack-2x"></i>}
+        </div>&nbsp;
+        <div style={labelText} className={!empty(this.props.checked) ? 'text-success' : 'text-muted'}>{this.props.label}</div>
+      </div>
+    )
+  }
+}
+
+BigCheckbox.propTypes = {
+  label: PropTypes.string,
+  checked: PropTypes.oneOfType([PropTypes.bool,PropTypes.string,PropTypes.number]),
+  handle: PropTypes.func.isRequired
+}
+
+BigCheckbox.defaultProps = {
+  checked: false
+}

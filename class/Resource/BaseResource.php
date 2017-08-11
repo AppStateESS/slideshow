a7e6a711a6058ae41bd2a3b065707f51f6ed2e22
Matthew McNaney <mcnaney@gmail.com>
2017-08-11 10:19:33 -0400

    Fixed (?) set logic

--- class/Resource/BaseResource.php
+++ class/Resource/BaseResource.php
@@ -14,7 +14,7 @@ class BaseResource extends \phpws2\Resource
 
     public function __set($name, $value)
     {
-        if ((!$this->$name->allowNull() ||
+        if ((!$this->$name->allowNull() &&
                 (method_exists($this->$name, 'allowEmpty') && !$this->$name->allowEmpty())) &&
                 ( (is_string($value) && $value === '') || is_null($value))) {
             throw new \properties\Exception\MissingInput("$name may not be empty");

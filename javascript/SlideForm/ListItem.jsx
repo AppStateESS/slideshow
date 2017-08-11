cc0456fbfd0d279b84bc47e9c2910e5ed627a812
Matthew McNaney <mcnaney@gmail.com>
2017-08-11 10:20:52 -0400

    decision form functionality added.

--- javascript/SlideForm/ListItem.jsx
+++ javascript/SlideForm/ListItem.jsx
@@ -7,11 +7,43 @@ export default class ListItem extends Component {
   constructor(props) {
     super(props)
     this.state = {}
+    this.view = this.view.bind(this)
+    this.edit = this.edit.bind(this)
+    this.deleteDecision = this.deleteDecision.bind(this)
   }
 
+  /* global $ */
+
+  deleteDecision(id) {
+    $.ajax({
+      url: './slideshow/Decision/' + id,
+      dataType: 'json',
+      type: 'delete',
+      success: function () {
+        this.props.load()
+      }.bind(this),
+      error: function () {}.bind(this),
+    })
+  }
+
+  view() {}
+
   render() {
-    const {slideKey, value, editSlide, deleteSlide, showForm} = this.props
+    const {slideKey, value, editSlide, deleteSlide, createDecision, editDecision} = this.props
     const DragHandle = SortableHandle(() => DragTag())
+    let decisions = <span>
+      <em>Continue normally</em>
+    </span>
+    if (value.decisions !== null) {
+      decisions = value.decisions.map(function (value, key) {
+        const clicks = {
+          editDecision: editDecision,
+          deleteDecision: this.deleteDecision,
+          view: this.view,
+        }
+        return <DecisionButton key={key} index={key} {...clicks} {...value}/>
+      }.bind(this))
+    }
     return (
       <li>
         <div>
@@ -35,9 +67,14 @@ export default class ListItem extends Component {
           <span>Slide {value.sorting}: {value.title}</span>
         </div>
         <hr/>
-        <div>
-          <button className="btn btn-primary" onClick={showForm}>
-            <i className="fa fa-plus"></i>&nbsp;Decision</button>
+        <div style={{
+          overflow: 'auto'
+        }}>
+          <button
+            className="btn btn-primary pull-right"
+            onClick={createDecision.bind(null, value.id)}>
+            <i className="fa fa-plus"></i>&nbsp;Choice</button>
+          {decisions}
         </div>
       </li>
     )
@@ -57,5 +94,32 @@ ListItem.propTypes = {
   value: PropTypes.object,
   editSlide: PropTypes.func,
   deleteSlide: PropTypes.func,
-  showForm: PropTypes.func,
+  createDecision: PropTypes.func,
+  editDecision: PropTypes.func,
+  load: PropTypes.func,
+}
+
+const DecisionButton = ({title, id, deleteDecision, editDecision, index}) => {
+  const _style = {
+    marginRight: '8px'
+  }
+  return (
+    <div className="btn-group" role="group" style={_style}>
+      <button className="btn btn-default">{title}</button>
+      <button className="btn btn-primary" onClick={editDecision.bind(null, index)}>
+        <i className="fa fa-edit"></i>
+      </button>
+      <button className="btn btn-danger" onClick={deleteDecision.bind(null, id)}>
+        <i className="fa fa-trash-o"></i>
+      </button>
+    </div>
+  )
+}
+
+DecisionButton.propTypes = {
+  id: PropTypes.string,
+  title: PropTypes.string,
+  editDecision: PropTypes.func,
+  deleteDecision: PropTypes.func,
+  view: PropTypes.func
 }

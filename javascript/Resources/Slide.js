3b0871f215525c288b968769201a7159ee019103
Matthew McNaney <mcnaney@gmail.com>
2017-08-16 13:47:25 -0400

    Made slide a class instead of a plain object

--- javascript/Resources/Slide.js
+++ javascript/Resources/Slide.js
@@ -1,11 +1,11 @@
-let Slide = {
-  id: 0,
-  delay : 0,
-  sectionId: 0,
-  sorting: 1,
-  title: 'Untitled slide',
-  content: '<p>Content here...</p>',
-  backgroundImage : ''
+export default class SlideObj {
+  constructor(slideId = 0) {
+    this.id = slideId
+    this.delay = 0
+    this.sectionId = 0
+    this.sorting = 1
+    this.title = 'Untitled slide'
+    this.content = '<p>Content here...</p>'
+    this.backgroundImage = ''
+  }
 }
-
-export default Slide

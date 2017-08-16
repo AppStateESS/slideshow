1f88611158ab8167bff48072a7282b9e5679ce16
Matthew McNaney <mcnaney@gmail.com>
2017-08-16 13:46:25 -0400

    broke up the slide and section admin

--- exports.js
+++ exports.js
@@ -5,5 +5,6 @@ exports.entry = {
   vendor: ['react', 'react-dom'],
   showform: exports.APP_DIR + '/ShowForm/index.jsx',
   sectionform: exports.APP_DIR + '/SectionForm/index.jsx',
-  slideform: exports.APP_DIR + '/SlideForm/index.jsx',
+  Section: exports.APP_DIR + '/Section/index.jsx',
+  Slide: exports.APP_DIR + '/Slide/index.jsx',
 }

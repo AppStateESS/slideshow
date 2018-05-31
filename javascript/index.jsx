f7acd8e3903a6756ebb34655064c28e6ef2359f2
tylercraig9332 <tmancraig03@gmail.com>
2018-05-31 14:20:52 -0400

    Restuctured some more directories

--- javascript/index.jsx
+++
@@ -1,7 +0,0 @@
-'use strict'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ShowView from 'ShowView.jsx'
-
-ReactDOM.render(
-  <ShowView/>, document.getElementById('ShowList'))

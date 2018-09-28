83b1c00f39763f7195eb6768aaa28a282d19013a
Olivia Perugini <oliviaperugini@gmail.com>
2018-09-28 15:21:33 -0400

    Delete branch later.

--- config/defines.dist.php
+++ config/defines.dist.php
@@ -1,11 +1,11 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-define('SLIDESHOW_REACT_DEV', false);
+define('SLIDESHOW_REACT_DEV', true);
 
 define('SS_FRIENDLY_ERROR', true);

36857e05ac34b33ed58e4327a520a694ac453cd2
Tyler Craig <tmancraig03@gmail.com>
2019-05-21 10:44:45 -0400

    Present view for quizzes (#74)
    * Addressed npm issues and updated dependencies
    
    * Created front-end view for presenting quizzes!!! :)
    
    * Changed the style of the radio buttons when editing a quiz
    
    * Added error support in the case an empty quiz slide is inserted
    
    * Better worded empty slide error message to help address the issue

--- javascript/Present/Present.jsx
+++ javascript/Present/Present.jsx
@@ -11,19 +11,67 @@ export default class Present extends Component {
       highestSlide: 0,
       content: [
         {
-          saveContent: undefined
+          saveContent: undefined,
+          quizContent: undefined
         }
       ],
       slideName: "Present: ",
+      // Flags:
+      prevDisable: true, // Disables prev button
+      nextDisable: true, // Disables next button
+      nextFlag: false, // true if user just changed slide
+      quizNextFlag: false, // the currentSlide is a quiz
+      quizPassed: false,
+      finishFlag: false // true when user has completed the show
     }
     this.load = this.load.bind(this)
 
     this.prev = this._prev.bind(this)
     this.next = this._next.bind(this)
+
+    this.validate = this.validate.bind(this)
+    this.returnToShowList = this.returnToShowList.bind(this)
+    this.parseBool = this.parseBool.bind(this)
   }
 
   componentDidMount() {
     this.load()
+  }
+
+  componentDidUpdate() {
+    if (this.state.quizNextFlag && (this.state.quizPassed || this.state.finishFlag)) {
+      // Correct answer chosen
+      this.setState({
+        quizNextFlag: false,
+        nextDisable: false,
+        quizPassed: false
+      })
+    }
+    else if (this.state.nextFlag) {
+      // SO this section is a beast -> There is probably a way to structure this better logically.
+      // There are a log of logical components that are changing and so the structure might not be the cleanest
+      // However, it works!!! Imma try my best to comment an explination for how this works - Tyler
+
+      // slide has changed
+      this.setState({nextDisable: true, prevDisable: true})
+      let isQuiz = this.parseBool(this.state.content[this.state.currentSlide].isQuiz) // Converts boolean if isQuiz is a string
+      let lastSlide = (this.state.currentSlide + 1 == this.state.content.length) // true if we are on last slide
+      if (isQuiz) {
+        this.setState({quizNextFlag: true}) // we changed to a quiz slide, handle the rest after user selects answer
+      }
+      else if (this.state.currentSlide == this.state.highestSlide && !this.state.finishFlag) { // we are on the highest slide and haven't finished the show yet
+        setTimeout(() => {
+          this.setState({nextDisable: false, finishFlag: (this.state.finishFlag || lastSlide)}) // on completion of timer, we unlock next and set finishFlag
+        }, 2000)
+      }
+      else if (this.state.currentSlide < this.state.highestSlide || this.state.finishFlag) { // we are on an already completed slide
+        this.setState({nextDisable: false})
+      }
+      if (this.state.currentSlide > 0) { // we are on the first slide
+        this.setState({prevDisable: false})
+      }
+      this.setState({nextFlag: false}) // we have handled the slide change
+    }
   }
 
   load() {
@@ -36,7 +84,15 @@ export default class Present extends Component {
         if (loaded != null) {
           this.setState({
             content: loaded,
-            slideName: data['title']
+            slideName: data['title'],
+            quizNextFlag: this.parseBool(loaded[0].isQuiz) // First slide is a quiz
+          }, () => {
+            if (!this.state.quizNextFlag) {
+              // First slide isn't a quiz we set timer on next
+              setTimeout(() => {
+                this.setState({nextDisable: false})
+              }, 2000)
+            }
           });
         }
       }.bind(this),
@@ -54,7 +110,8 @@ export default class Present extends Component {
     }
     this.setState({
       currentSlide: slideNum,
-      highestSlide: highest
+      highestSlide: highest,
+      nextFlag: true
     })
   }
 
@@ -68,6 +125,26 @@ export default class Present extends Component {
     if (this.state.currentSlide != this.state.content.length - 1) {
       this.changeSlide(this.state.currentSlide + 1)
     }
+  }
+
+  validate() {
+    /* this function is only called if the correct answer is chosen */
+    //if (this.state.content[this.state.currentSlide].correctAnswerIndex == answer) {
+    let finish = this.state.finishFlag
+    if (this.state.content.length == this.state.currentSlide + 1) {
+      finish = true
+    }
+    this.setState({quizPassed: true, finishFlag: finish})
+    //}
+  }
+
+  returnToShowList() {
+    window.location.href = './slideshow/Show/list'
+  }
+
+  parseBool(bool) {
+    if (bool == undefined) return false
+    return (typeof(bool) == "boolean") ? bool : JSON.parse(bool)
   }
 
   render() {
@@ -85,6 +162,13 @@ export default class Present extends Component {
         )
       }
     }.bind(this));
+    /*console.log("curr: " + this.state.currentSlide)
+    console.log("len: " + this.state.content.length)
+    console.log(this.state.currentSlide + 1 == this.state.content.length)*/
+    let nextButton = (this.state.finishFlag) ?
+      <button key="finish" className="btn btn-secondary" onClick={this.returnToShowList} disabled={this.state.nextDisable}>Finish</button> :
+      <button key="next" className="btn btn-secondary" onClick={this.next} disabled={this.state.nextDisable}>Next</button>
+
     return(
       <div>
         <h1 style={{textDecorationLine: 'underline'}}>{this.state.slideName}</h1>
@@ -93,15 +177,19 @@ export default class Present extends Component {
           <div style={{maxWidth: 700, width: "95%"}}>
             <PresentView
               currentSlide={this.state.currentSlide}
-              content={this.state.content[this.state.currentSlide]} />
+              high={this.state.highestSlide}
+              content={this.state.content[this.state.currentSlide]}
+              nextSlide={this.next}
+              validate={this.validate}
+              />
           </div>
         </div>
         <div style={{justifyContent: 'center', display: 'flex', marginBottom: '2rem'}}>
           <div className="btn-toolbar">
             <div className="btn-group">
-              <button className="btn btn-secondary" onClick={this.prev}>Previous</button>
+              <button className="btn btn-secondary" onClick={this.prev} disabled={this.state.prevDisable}>Previous</button>
               {slidesButtons}
-              <button className="btn btn-secondary" onClick={this.next}>Next</button>
+              {nextButton}
             </div>
           </div>
         </div>
---
+++ javascript/Present/QuizViewspace.jsx
@@ -0,0 +1,129 @@
+'use strict'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import {
+  Alert,
+  Form,
+  InputGroup
+} from 'react-bootstrap'
+
+export default class QuizViewspace extends Component {
+
+  constructor() {
+    super()
+
+    this.state = {
+      answers: undefined,
+      correct: false,
+      incorrect: false
+    }
+
+    this.validate = this.validate.bind(this)
+    this.buildAnswerComponent = this.buildAnswerComponent.bind(this)
+    this.toggleCorrectAlert = this._toggleCorrect.bind(this)
+    this.toggleIncorrectAlert = this._toggleIncorrect.bind(this)
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.currentSlide != prevProps.currentSlide) {
+      this.setState({correct:false, incorrect:false})
+    }
+  }
+
+  validate(event) {
+    let ids = event.target.id.split('-')
+    if (ids[0] === 'select') {
+      // MultipleChoice
+      if (ids[1] == this.props.content.quizContent.correctAnswerIndex) {
+        this.props.validate()
+        this.toggleCorrectAlert()
+      }
+      else {
+        this.toggleIncorrectAlert()
+      }
+    }
+  }
+
+  _toggleCorrect() {
+    this.setState({correct: true, incorrect: false})
+  }
+
+  _toggleIncorrect() {
+    this.setState({incorrect: true})
+  }
+
+  buildAnswerComponent() {
+    if (this.props.content.quizContent != undefined) {
+      let i = -1
+      let a = this.props.content.quizContent.answers.map((answer) => {
+        i += 1
+
+        // Shows a check if the answer has been correctly answered before
+        let c = this.props.content.quizContent.correctAnswerIndex
+        let ans = ((this.props.currentSlide < this.props.highestSlide) && (c == i)) ?
+          (<span>{answer} <i className="fas fa-check-circle" style={{color: 'green'}}></i></span>) : answer
+
+        return (
+          <div key={`answer-${i}-${this.props.currentSlide}`} className='mb-3'>
+            <Form.Check
+              custom
+              type='radio'
+              id={`select-${i}`}
+              name={`answers-${this.props.currentSlide}`}
+              label={ans}
+              onClick={this.validate}
+            />
+          </div>)
+      })
+      return (
+        <Form>
+          {a}
+        </Form>
+      )
+    }
+  }
+
+  render() {
+    let alert = undefined
+    if (this.state.correct) {
+      alert = (<Alert key={this.state.currentSlide} variant="success">
+                Correct! <div className="spinner-border text-primary"></div>
+               </Alert>)
+    }
+    else if (this.state.incorrect) {
+      alert = (<Alert key={this.state.currentSlide} variant="danger">Please try again</Alert>)
+    }
+    else if (this.props.currentSlide == this.props.highestSlide) {
+      alert = <Alert key={this.state.currentSlide} variant="dark">Select the correct answer to continue</Alert>
+    }
+
+    let answersComponent = undefined
+    let titleComponent = undefined
+    if (this.props.content.quizContent == undefined) {
+      titleComponent = "Error - Empty Quiz"
+      answersComponent = (<div>
+                            <p style={{color: 'red'}}>This quiz slide has not been filled with data</p>
+                            <p>If you are a student, contact the one responsible for making you take this</p>
+                            <p>If you are an admin, fill out this slide with data</p>
+                          </div>)
+      alert = undefined
+      //this.validate(null) This could allow user to continue
+    }
+    else {
+     answersComponent = this.buildAnswerComponent()
+     titleComponent = this.props.content.quizContent.questionTitle
+    }
+    return (
+      <div>
+        <h3>{titleComponent}:</h3>
+        {answersComponent}
+        {alert}
+      </div>)
+  }
+}
+
+QuizViewspace.propTypes = {
+  content: PropTypes.object,
+  currentSlide: PropTypes.number,
+  highestSlide: PropTypes.number
+}

a4dd540b7ce07a73c0b7af6afc4d8d6277509dc7
zanoble <zanoble327@gmail.com>
2019-07-10 11:01:54 -0400

    pointers for hover on sort and sort for username

--- javascript/Session/SessionTable.jsx
+++ javascript/Session/SessionTable.jsx
@@ -9,13 +9,15 @@ export default class SessionTable extends Component {
     super(props)
 
     this.state = {
-      userData: null,
       sessionFlag: null,
-      sortStatus: false
+      sortStatus: true,
+      sortName: false
     }
         this.getSessionInfo = this.getSessionInfo.bind(this)
         this.sortStatus = this.sortStatus.bind(this)
-        this.compare = this.compare.bind(this)
+        this.compareStatus = this.compareStatus.bind(this)
+        this.sortUsername = this.sortUsername.bind(this)
+        this.compareUsername = this.compareUsername.bind(this)
   }
 
   componentDidMount() {
@@ -28,20 +30,21 @@ export default class SessionTable extends Component {
       type: 'GET',
       dataType: 'json',
       success: function (data) {
-        this.setState({showData: data})
+        this.setState({showData: data}, () => this.sortStatus())
       }.bind(this)
     })
   }
 
+  // sorts data either completed to not started or not started to completed
   sortStatus() {
     let sortData = [...this.state.showData]
     sortData.sort((b, a) => {
-        return this.compare(b, a)
+        return this.compareStatus(b, a)
     })
     this.setState({showData: sortData, sortStatus: !this.state.sortStatus})
   }
 
-  compare(b, a) {
+  compareStatus(b, a) {
       if (this.state.sortStatus){
           let x = b
           b = a
@@ -66,6 +69,32 @@ export default class SessionTable extends Component {
           return 0
         }
       }
+  }
+
+  sortUsername() {
+    let sortData = [...this.state.showData]
+    sortData.sort((a,b) => {
+      return this.compareUsername(a,b)
+    })
+    this.setState({showData: sortData, sortName: !this.state.sortName})
+  }
+
+  compareUsername(a, b) {
+    if (this.state.sortName){
+        let x = b
+        b = a
+        a = x
+    }
+    //logic
+    if(a.username < b.username) {
+      return -1
+    }
+    else if(a.username > b.username) {
+      return 1
+    }
+    else {
+      return 0
+    }
   }
 
   render() {
@@ -94,11 +123,11 @@ export default class SessionTable extends Component {
     return (
       <div>
       <h1 style={{padding: 10}}><u>{window.sessionStorage.getItem('title')}</u></h1>
-      <Table striped bordered>
+      <Table striped bordered hover>
         <thead>
           <tr>
-            <th>Username</th>
-            <th onClick= {this.sortStatus}>Status <i className="fas fa-sort"></i></th>
+            <th onClick= {this.sortUsername}><div className= "status-arrows">Username <i className="fas fa-sort"></i></div></th>
+            <th onClick= {this.sortStatus}><div className= "status-arrows">Status <i className="fas fa-sort"></i></div></th>
           </tr>
         </thead>
         <tbody>

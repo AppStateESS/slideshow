a9f6100b61613c33e91a0834093cf22596bf20ad
tylercraig9332 <tmancraig03@gmail.com>
2019-07-22 17:36:42 -0400

    added caching to alignment to address clunkyness

--- javascript/Edit/Toolbar/Alignment.jsx
+++ javascript/Edit/Toolbar/Alignment.jsx
@@ -10,6 +10,8 @@ export default class Alignment extends Component {
   constructor(props) {
     super(props)
 
+    this.state = {selection: SelectionState.createEmpty()}
+
 
     this.align = this.align.bind(this)
   }
@@ -17,38 +19,48 @@ export default class Alignment extends Component {
   align(event) {
     let toggledAlignment = 'align-' + event.currentTarget.id
 
-    const editorState = this.props.getEditorState() 
+    const editorState = this.props.getEditorState()
+    let newEditorState = editorState  
     let selection = editorState.getSelection()
 
-    // remove old alignment
-    let newContentState = Object.keys(AlignmentMap)
-      .reduce((contentState, alignment) => {
-          return Modifier.removeInlineStyle(contentState, selection, alignment)
-      }, editorState.getCurrentContent())
-    
-    let newEditorState = EditorState.push(
-      editorState,
-      newContentState,
-      'change-inline-style'
-    )
-
-
-    // If nothing is currently selected, then we align the block
+    // If nothing is currently selected
     if (selection.isCollapsed()) {
-      // Note: this alignment will only apply to things that have no alignment already
-      // So, if something has been aligned through select then things get deselected,
-      // it won't align unless reselected. Consider this an align all
-      const contentState = newEditorState.getCurrentContent()
-      const anchorKey = selection.getAnchorKey()
-      const currBlock = contentState.getBlockForKey(anchorKey)
-      const bData = currBlock.getData().set('align', toggledAlignment)
-      const newContentState = Modifier.setBlockData(contentState, selection, bData)
+      // if the cached selection is undefined, then we align the block
+      if (this.state.selection.isCollapsed()) {
+        // Note: this alignment will only apply to things that have no alignment already
+        // So, if something has been aligned through select then things get deselected,
+        // it won't align unless reselected. Consider this an align all
+        const contentState = editorState.getCurrentContent()
+        const anchorKey = selection.getAnchorKey()
+        const currBlock = contentState.getBlockForKey(anchorKey)
+        const bData = currBlock.getData().set('align', toggledAlignment)
+        const newContentState = Modifier.setBlockData(contentState, selection, bData)
+        newEditorState = EditorState.push(
+          editorState,
+          newContentState,
+          'change-block-data'
+        )
+      }
+      else {
+        // retrieve cached selection
+        selection = this.state.selection
+      }
+
+    }
+    if (!selection.isCollapsed()) {
+      // remove old alignment
+      let newContentState = Object.keys(AlignmentMap)
+        .reduce((contentState, alignment) => {
+            return Modifier.removeInlineStyle(contentState, selection, alignment)
+        }, editorState.getCurrentContent())
+      
       newEditorState = EditorState.push(
-        newEditorState,
+        editorState,
         newContentState,
-        'change-block-data'
+        'change-inline-style'
       )
     }
+      
 
     const currStyle = editorState.getCurrentInlineStyle() 
 
@@ -56,6 +68,8 @@ export default class Alignment extends Component {
       newEditorState = RichUtils.toggleInlineStyle(newEditorState, toggledAlignment)
     }
 
+    // cache the selection
+    this.setState({selection: selection})
     this.props.setEditorState(EditorState.moveFocusToEnd(newEditorState))
   }
 

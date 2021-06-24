e4f8cfc9abd7168cb84f6a5c7e15120bf0f614ae
Matt McNaney <mcnaneym@appstate.edu>
2021-06-24 10:08:43 -0400

    Formatting and proptypes.

--- javascript/Edit/EditView.jsx
+++ javascript/Edit/EditView.jsx
@@ -1,39 +1,100 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 
 import QuizEdit from './Quiz/QuizEdit.jsx'
 import QuizView from './Quiz/QuizView.jsx'
 import Editor from './DraftEditor'
 import ToolbarQ from './Toolbar/QuizToolbar.jsx'
 
+const EditView = ({
+  content,
+  currentSlide,
+  saveContentState,
+  saveMedia,
+  removeMedia,
+  saveBackground,
+  saveQuizContent,
+  load,
+}) => {
+  const [editView, setEditView] = useState(false)
+  //const [mediaAlign, setMediaAlign] = useState('right')
+  const mediaAlign = 'right'
 
-export default function EditView(props) {
-
-    const [editView, setEditView] = useState(false)
-    const [mediaAlign, setMediaAlign] = useState('right')
-
-    let imgRender = undefined // Note This does nothing for now, but when we want to add support for images on quizzes this will be here.
-    if (!props.content.isQuiz) return <Editor key={props.currentSlide} content={props.content} saveContentState={props.saveContentState} currentSlide={props.currentSlide} saveMedia={props.saveMedia} removeMedia={props.removeMedia} saveBackground={props.saveBackground}/>
-    if (editView || props.content.quizContent == null) return (
-        <div className="col" style={{marginTop: 12}}>
-            <ToolbarQ toggleQuizEdit={() => setEditView(!editView)} view={editView} />
-            <span id="space_between_the_toolbar" style={{padding: 1}} ></span>
-            <QuizEdit quizContent={props.content.quizContent} saveQuizContent={props.saveQuizContent} toggle={() => setEditView(false)} load={props.load}/>
-        </div>)
+  let imgRender = undefined // Note This does nothing for now, but when we want to add support for images on quizzes this will be here.
+  if (!content.isQuiz) {
     return (
-        <div className="col" style={{ paddingTop: 12}}>
-            <div style={{minWidth: 700}}>
-                <ToolbarQ toggleQuizEdit={() => setEditView(!editView)} view={editView} />
-                <span id="space_between_the_toolbar" style={{padding: 1}} ></span>
-                <div id="editor" data-key={props.currentSlide} className="jumbotron" style={{ minHeight: 500, minWidth: 300, height: '8rem', backgroundColor: props.content.background, overflow:'auto'}}>
-                    <div className="row">
-                    {(mediaAlign === 'left') ? imgRender : undefined}
-                    <div className="col">
-                        <QuizView quizContent={props.content.quizContent} toggle={() => setEditView(true)}/>
-                    </div>
-                    {(mediaAlign === 'right') ? imgRender : undefined}
-                    </div>
-                </div>
-            </div>
-        </div>
+      <Editor
+        key={currentSlide}
+        content={content}
+        saveContentState={saveContentState}
+        currentSlide={currentSlide}
+        saveMedia={saveMedia}
+        removeMedia={removeMedia}
+        saveBackground={saveBackground}
+      />
     )
+  }
+  if (editView || content.quizContent == null) {
+    return (
+      <div className="col" style={{marginTop: 12}}>
+        <ToolbarQ
+          toggleQuizEdit={() => setEditView(!editView)}
+          view={editView}
+        />
+        <span id="space_between_the_toolbar" style={{padding: 1}}></span>
+        <QuizEdit
+          quizContent={content.quizContent}
+          saveQuizContent={saveQuizContent}
+          toggle={() => setEditView(false)}
+          load={load}
+        />
+      </div>
+    )
+  }
+  return (
+    <div className="col" style={{paddingTop: 12}}>
+      <div style={{minWidth: 700}}>
+        <ToolbarQ
+          toggleQuizEdit={() => setEditView(!editView)}
+          view={editView}
+        />
+        <span id="space_between_the_toolbar" style={{padding: 1}}></span>
+        <div
+          id="editor"
+          data-key={currentSlide}
+          className="jumbotron"
+          style={{
+            minHeight: 500,
+            minWidth: 300,
+            height: '8rem',
+            backgroundColor: content.background,
+            overflow: 'auto',
+          }}>
+          <div className="row">
+            {mediaAlign === 'left' ? imgRender : undefined}
+            <div className="col">
+              <QuizView
+                quizContent={content.quizContent}
+                toggle={() => setEditView(true)}
+              />
+            </div>
+            {mediaAlign === 'right' ? imgRender : undefined}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
+
+EditView.propTypes = {
+  content: PropTypes.object,
+  currentSlide: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  saveMedia: PropTypes.func,
+  removeMedia: PropTypes.func,
+  saveContentState: PropTypes.func,
+  saveBackground: PropTypes.func,
+  saveQuizContent: PropTypes.func,
+  load: PropTypes.func,
+}
+
+export default EditView

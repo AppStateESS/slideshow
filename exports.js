e6aa114163e266feedfde83e98b4c0ddf85a4e23
Matt McNaney <mcnaneym@appstate.edu>
2021-06-24 09:30:23 -0400

    Altered to work with new webpage.

--- exports.js
+++ exports.js
@@ -1,12 +1,11 @@
+/* global __dirname, exports */
 exports.path = require('path')
 exports.APP_DIR = exports.path.resolve(__dirname, 'javascript')
 
 exports.entry = {
-  vendor: ['react', 'react-dom'],
-
   shows: exports.APP_DIR + '/Show/index.jsx',
   edit: exports.APP_DIR + '/Edit/index.jsx',
   present: exports.APP_DIR + '/Present/index.jsx',
   view: exports.APP_DIR + '/View/index.jsx',
-  session: exports.APP_DIR + '/Session/index.jsx'
+  session: exports.APP_DIR + '/Session/index.jsx',
 }

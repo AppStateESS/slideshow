efd21ce11815e9b9daeeb4f9425f1783dff92c60
Matt McNaney <mcnaneym@appstate.edu>
2021-06-24 09:30:50 -0400

    Updated webpack to work with new packages.

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,104 @@
-var setup = require('./exports.js')
 var webpack = require('webpack')
-var Promise = require('es6-promise').polyfill()
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+var setup = require('./exports.js')
+var BrowserSyncPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  entry: setup.entry,
-  output: {
-    path: setup.path.join(setup.APP_DIR, "dev"),
-    filename: "[name].js"
-  },
-  resolve: {
-    extensions: [
-      '.js', '.jsx',
-    ],
-  },
-  plugins: [
-    new webpack.ProvidePlugin({$: "jquery", jQuery: "jquery", }),
-    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.js'}),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      files: ['./javascript/dev/*.js'],
-      proxy: 'localhost:8080/slideshow'
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        enforce: 'pre',
-        loader: 'jshint-loader',
-        exclude: '/node_modules/',
-        include: setup.APP_DIR + "/dev"
-      }, {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
-      }, {
-        test: /\.jsx?/,
-        include: setup.APP_DIR,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env', 'react', ]
-        }
-      }, {
-        test: /\.(s*)css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+/* global module */
+
+module.exports = (env, argv) => {
+  const inProduction = argv.mode === 'production'
+  const inDevelopment = argv.mode === 'development'
+
+  const settings = {
+    entry: setup.entry,
+    output: {
+      path: setup.path.join(setup.APP_DIR, 'dev'),
+    },
+    watchOptions: {ignored: /node_modules/},
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          defaultVendors: {
+            test: /[\\/]node_modules[\\/]/,
+            minChunks: 3,
+            name: 'vendor',
+            enforce: true,
+            chunks: 'all',
+          },
+        },
       },
-    ]
-  },
-  devtool: 'source-map'
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    plugins: [new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'})],
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          use: {loader: 'url-loader?limit=100000'},
+        },
+        {
+          test: /\.jsx?/,
+          include: setup.APP_DIR,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        },
+      ],
+    },
+  }
+
+  if (inDevelopment) {
+    const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+    settings.plugins.push(
+      new BrowserSyncPlugin({
+        host: 'localhost',
+        notify: false,
+        port: 3000,
+        files: ['./javascript/dev/*.js'],
+        proxy: 'localhost/canopy',
+      })
+    )
+
+    settings.devtool = 'inline-source-map'
+    settings.output = {
+      path: setup.path.join(setup.APP_DIR, 'dev'),
+      filename: '[name].js',
+    }
+  }
+
+  if (inProduction) {
+    // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    //   .BundleAnalyzerPlugin
+    // settings.plugins.push(new BundleAnalyzerPlugin())
+    settings.optimization.minimize = true
+    settings.optimization.minimizer = [new TerserPlugin()]
+
+    const AssetsPlugin = require('assets-webpack-plugin')
+    settings.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production'),
+      })
+    )
+    settings.plugins.push(
+      new AssetsPlugin({
+        filename: 'assets.json',
+        prettyPrint: true,
+        removeFullPathAutoPrefix: true,
+      })
+    )
+    settings.output = {
+      path: setup.path.join(setup.APP_DIR, 'build'),
+      filename: '[name].[chunkhash:8].min.js',
+      chunkFilename: '[name].[chunkhash:8].chunk.js',
+    }
+  }
+  return settings
 }

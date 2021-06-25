27ac0e8a934fb1bdef5e2d6b21619d6386610dc2
Matt McNaney <mcnaneym@appstate.edu>
2021-06-25 13:55:16 -0400

    Cleaned up formatting.
    Changed h1 to h2.

--- javascript/Session/SessionTable.jsx
+++ javascript/Session/SessionTable.jsx
@@ -1,8 +1,9 @@
 'use strict'
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { Table } from 'react-bootstrap'
+import React, {Component} from 'react'
+import {Table} from 'react-bootstrap'
 import './custom.css'
+
+/* global $ */
 
 export default class SessionTable extends Component {
   constructor(props) {
@@ -11,13 +12,13 @@ export default class SessionTable extends Component {
     this.state = {
       sessionFlag: null,
       sortStatus: true,
-      sortName: false
+      sortName: false,
     }
-        this.getSessionInfo = this.getSessionInfo.bind(this)
-        this.sortStatus = this.sortStatus.bind(this)
-        this.compareStatus = this.compareStatus.bind(this)
-        this.sortUsername = this.sortUsername.bind(this)
-        this.compareUsername = this.compareUsername.bind(this)
+    this.getSessionInfo = this.getSessionInfo.bind(this)
+    this.sortStatus = this.sortStatus.bind(this)
+    this.compareStatus = this.compareStatus.bind(this)
+    this.sortUsername = this.sortUsername.bind(this)
+    this.compareUsername = this.compareUsername.bind(this)
   }
 
   componentDidMount() {
@@ -31,7 +32,7 @@ export default class SessionTable extends Component {
       dataType: 'json',
       success: function (data) {
         this.setState({showData: data}, () => this.sortStatus())
-      }.bind(this)
+      }.bind(this),
     })
   }
 
@@ -39,60 +40,54 @@ export default class SessionTable extends Component {
   sortStatus() {
     let sortData = [...this.state.showData]
     sortData.sort((b, a) => {
-        return this.compareStatus(b, a)
+      return this.compareStatus(b, a)
     })
     this.setState({showData: sortData, sortStatus: !this.state.sortStatus})
   }
 
   compareStatus(b, a) {
-      if (this.state.sortStatus){
-          let x = b
-          b = a
-          a = x
+    if (this.state.sortStatus) {
+      let x = b
+      b = a
+      a = x
+    }
+    // order logic
+    if (Number(a.highestSlide) == 0) {
+      return 1 //a is not Started
+    } else if (a.completed == 1) {
+      return -1 //a is completed
+    } else {
+      //a is in progress
+      if (b.completed == 1) {
+        return 1
+      } else if (b.highestSlide == 0) {
+        return -1
+      } else {
+        return 0
       }
-      // order logic
-      if (Number(a.highestSlide) == 0) {
-        return 1 //a is not Started
-      }
-      else if (a.completed == 1) {
-        return -1 //a is completed
-      }
-      else {
-        //a is in progress
-        if (b.completed == 1) {
-          return 1
-        }
-        else if (b.highestSlide == 0) {
-          return -1
-        }
-        else {
-          return 0
-        }
-      }
+    }
   }
 
   sortUsername() {
     let sortData = [...this.state.showData]
-    sortData.sort((a,b) => {
-      return this.compareUsername(a,b)
+    sortData.sort((a, b) => {
+      return this.compareUsername(a, b)
     })
     this.setState({showData: sortData, sortName: !this.state.sortName})
   }
 
   compareUsername(a, b) {
-    if (this.state.sortName){
-        let x = b
-        b = a
-        a = x
+    if (this.state.sortName) {
+      let x = b
+      b = a
+      a = x
     }
     //logic
-    if(a.username < b.username) {
+    if (a.username < b.username) {
       return -1
-    }
-    else if(a.username > b.username) {
+    } else if (a.username > b.username) {
       return 1
-    }
-    else {
+    } else {
       return 0
     }
   }
@@ -100,20 +95,47 @@ export default class SessionTable extends Component {
   render() {
     let tableData = undefined
     let status = undefined
-    if (this.state.showData != null){
+    if (this.state.showData != null) {
       tableData = this.state.showData.map((row) => {
         if (Number(row.highestSlide) == 0) {
-          status = <td><div style = {{color: '#dc3545'}}><i className="fas fa-times-circle" style= {{marginRight: 10}}></i>Not started</div></td>
-        }
-        else if (Number(row.completed)) {
-          status = <td><div style = {{color: '#28a745'}}><i className="fas fa-check-circle" style= {{marginRight: 10}}></i>Complete</div></td>
-        }
-        else if(Number(row.highestSlide) > 0) {
-          status= <td><div style = {{color: '#ffc107'}}><i className="fas fa-exclamation-circle" style= {{marginRight: 10}}></i>In progress</div></td>
+          status = (
+            <td>
+              <div style={{color: '#dc3545'}}>
+                <i
+                  className="fas fa-times-circle"
+                  style={{marginRight: 10}}></i>
+                Not started
+              </div>
+            </td>
+          )
+        } else if (Number(row.completed)) {
+          status = (
+            <td>
+              <div style={{color: '#28a745'}}>
+                <i
+                  className="fas fa-check-circle"
+                  style={{marginRight: 10}}></i>
+                Complete
+              </div>
+            </td>
+          )
+        } else if (Number(row.highestSlide) > 0) {
+          status = (
+            <td>
+              <div style={{color: '#ffc107'}}>
+                <i
+                  className="fas fa-exclamation-circle"
+                  style={{marginRight: 10}}></i>
+                In progress
+              </div>
+            </td>
+          )
         }
         return (
           <tr key={row.username}>
-            <td><span>{row.username}</span></td>
+            <td>
+              <span>{row.username}</span>
+            </td>
             {status}
           </tr>
         )
@@ -122,18 +144,26 @@ export default class SessionTable extends Component {
 
     return (
       <div>
-      <h1 style={{padding: 10}}><u>{window.sessionStorage.getItem('title')}</u></h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th onClick= {this.sortUsername}><div className= "status-arrows">Username <i className="fas fa-sort"></i></div></th>
-            <th onClick= {this.sortStatus}><div className= "status-arrows">Status <i className="fas fa-sort"></i></div></th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableData}
-        </tbody>
-      </Table>
+        <h2 style={{padding: 10}}>
+          <u>{window.sessionStorage.getItem('title')}</u>
+        </h2>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th onClick={this.sortUsername}>
+                <div className="status-arrows">
+                  Username <i className="fas fa-sort"></i>
+                </div>
+              </th>
+              <th onClick={this.sortStatus}>
+                <div className="status-arrows">
+                  Status <i className="fas fa-sort"></i>
+                </div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>{tableData}</tbody>
+        </Table>
       </div>
     )
   }

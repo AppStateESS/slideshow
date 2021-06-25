edb4acc6a509a901705f25f5e2230925b8955746
Matt McNaney <mcnaneym@appstate.edu>
2021-06-25 13:58:28 -0400

    Added light-border theme to Tippy

--- javascript/Show/SessionTool.jsx
+++ javascript/Show/SessionTool.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Tippy from '@tippyjs/react'
+import 'tippy.js/themes/light-border.css'
+import PropTypes from 'prop-types'
+SessionTool.propTypes = {
+  sessionTransition: PropTypes.func,
+}
 
 export default function SessionTool(props) {
   return (
-    <Tippy content={<div>View user progress</div>} arrow={true}>
+    <Tippy
+      theme="light-border"
+      content={<div>View user progress</div>}
+      arrow={true}>
       <button className="tool" onClick={props.sessionTransition}>
         <i className="fas fa-users"></i>
       </button>

b9bdb5274b089cd829df3a3b75f5cf18134760a9
Matt McNaney <mcnaneym@appstate.edu>
2021-06-25 13:52:37 -0400

    Formatting.

--- javascript/Present/Present.jsx
+++ javascript/Present/Present.jsx
@@ -1,151 +1,181 @@
-import React, { useState, useEffect } from 'react'
+import React, {useState, useEffect} from 'react'
 
 // Resources for loading from db
-import { fetchShow, fetchSlides, fetchSession, updateSession, slidesResource } from '../api/present'
+import {
+  fetchShow,
+  fetchSlides,
+  fetchSession,
+  updateSession,
+  slidesResource,
+} from '../api/present'
 
 import PresentView from './PresentView'
 
-import { Progress, Navigation, Finish, SlidesNav } from './Navbar'
+import {Progress, Navigation, Finish, SlidesNav} from './Navbar'
 import Skeleton from '../Resources/Components/Skeleton'
 
 import 'animate.css'
 
 export default function Present() {
-    
-    const [showTitle, setShowTitle] = useState('Present: ')
-    const [showTimer, setShowTimer] = useState(0)
-    const [showAnimation, setShowAnimation] = useState('None')
+  const [showTitle, setShowTitle] = useState('Present: ')
+  const [showTimer, setShowTimer] = useState(0)
+  const [showAnimation, setShowAnimation] = useState('None')
 
-    const [content, setContent] = useState(slidesResource.content)
+  const [content, setContent] = useState(slidesResource.content)
 
-    const [currentSlide, setCurrentSlide] = useState(0)
-    const [highestSlide, setHighestSlide] = useState(0)
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [highestSlide, setHighestSlide] = useState(0)
 
-    const [prevDisable, setPrevDisable] = useState(true) // previous button disabled?
-    const [nextDisable, setNextDisable] = useState(true) // next button disabled?
+  const [prevDisable, setPrevDisable] = useState(true) // previous button disabled?
+  const [nextDisable, setNextDisable] = useState(true) // next button disabled?
 
-    const [loaded, setLoaded] = useState(false) // use to avoid running logic before db calls return
-    const [finished, setFinished] = useState(false)
+  const [loaded, setLoaded] = useState(false) // use to avoid running logic before db calls return
+  const [finished, setFinished] = useState(false)
 
-    /** Component did mount */
-    useEffect(() => {
-        load()
-    }, [])
+  /** Component did mount */
+  useEffect(() => {
+    load()
+  }, [])
 
-    useEffect(() => {
-        // "Touple" as array of the values
-        // of [prevDisable, nextDisable, highestSlide]
-        const state = evaluateState()
-        setNextDisable(state[1])
-        setPrevDisable(state[0])
-        const high = state[2]
-        setHighestSlide(high)
-        //TODO: Update session with new highestSlide / currentSlide (maybe?)
-        const finish = (high == content.length-1)
-        updateSession(window.sessionStorage.getItem('id'), high, finish)
-    }, [currentSlide])
+  useEffect(() => {
+    // "Touple" as array of the values
+    // of [prevDisable, nextDisable, highestSlide]
+    const state = evaluateState()
+    setNextDisable(state[1])
+    setPrevDisable(state[0])
+    const high = state[2]
+    setHighestSlide(high)
+    //TODO: Update session with new highestSlide / currentSlide (maybe?)
+    const finish = high == content.length - 1
+    updateSession(window.sessionStorage.getItem('id'), high, finish)
+  }, [currentSlide])
 
-    useEffect(() => {
-        // This will run before the data loads from db this eliminates that run
-        if (!loaded) return
-        const visited = currentSlide < highestSlide
-        const isQuiz = content[currentSlide].isQuiz
-        const final = currentSlide === content.length - 1 
-        if (nextDisable && loaded && !visited && !isQuiz) {
-            window.setTimeout(() => {
-                setNextDisable(final)
-                setFinished(final)
-            }, showTimer)
-        }
-    }, [nextDisable])
-
-    async function load() {
-        const showId = window.sessionStorage.getItem('id')
-        const show = await fetchShow(showId)
-        const content = await fetchSlides(showId)
-        const session = await fetchSession(showId)
-
-        let current = Number(session.highest)
-        if (session.complete) {
-            current = 0
-        }
-        setShowTitle(show.showTitle)
-        setShowTimer(show.showTimer)
-        setShowAnimation(show.animation)
-        setContent(content)
-        setCurrentSlide(current)
-        setHighestSlide(Number(session.highest))
-        setLoaded(true)
-        setFinished(session.complete)
-
-        setNextDisable(!session.complete)
-        window.setTimeout(() => {
-            setNextDisable(false)
-        }, show.showTimer)
-    }
-
-    function evaluateState() {
-        // Handle behavior for next and prev disables
-        let next = true
-        let prev = false
-        let high = highestSlide
-        if (high < currentSlide) high = currentSlide
-        const visited = (currentSlide < highestSlide)
-        if (finished || visited) {
-            next = false
-        }
-
-        // Final global check that will exist despite finished
-        if (currentSlide == 0) {
-            // First slide
-            prev = true
-        } 
-        else if (currentSlide == content.length - 1) {
-            // Last slide
-            next = true
-        }
-        return [prev, next, high]
-    }
-
-    function validate() {
-        // This will be called from subcompents to revalidate the current component
-        let final = false
-        if (currentSlide === content.length - 1) final = true
+  useEffect(() => {
+    // This will run before the data loads from db this eliminates that run
+    if (!loaded) return
+    const visited = currentSlide < highestSlide
+    const isQuiz = content[currentSlide].isQuiz
+    const final = currentSlide === content.length - 1
+    if (nextDisable && loaded && !visited && !isQuiz) {
+      window.setTimeout(() => {
         setNextDisable(final)
-        
+        setFinished(final)
+      }, showTimer)
+    }
+  }, [nextDisable])
+
+  async function load() {
+    let showId = window.sessionStorage.getItem('id')
+    if (showId == null) {
+      const params = new URLSearchParams(window.location.search)
+      showId = params.get('id')
+      if (showId > 0) {
+        window.sessionStorage.setItem('id', showId)
+      }
+    }
+    const show = await fetchShow(showId)
+    const content = await fetchSlides(showId)
+    const session = await fetchSession(showId)
+
+    let current = Number(session.highest)
+    if (session.complete) {
+      current = 0
+    }
+    setShowTitle(show.showTitle)
+    setShowTimer(show.showTimer)
+    setShowAnimation(show.animation)
+    setContent(content)
+    setCurrentSlide(current)
+    setHighestSlide(Number(session.highest))
+    setLoaded(true)
+    setFinished(session.complete)
+
+    setNextDisable(!session.complete)
+    window.setTimeout(() => {
+      setNextDisable(false)
+    }, show.showTimer)
+  }
+
+  function evaluateState() {
+    // Handle behavior for next and prev disables
+    let next = true
+    let prev = false
+    let high = highestSlide
+    if (high < currentSlide) high = currentSlide
+    const visited = currentSlide < highestSlide
+    if (finished || visited) {
+      next = false
     }
 
-    function _next() {
-        let next = currentSlide + 1
-        if (next == content.length) next = currentSlide // set disable
-        setCurrentSlide(next) 
+    // Final global check that will exist despite finished
+    if (currentSlide == 0) {
+      // First slide
+      prev = true
+    } else if (currentSlide == content.length - 1) {
+      // Last slide
+      next = true
     }
+    return [prev, next, high]
+  }
 
-    function _prev() {
-        let prev = currentSlide - 1 
-        if (prev < 0) prev = 0 // set state to disable
-        setCurrentSlide(prev) 
-    }
-    if (!loaded) return <Skeleton />
-    return (
+  function validate() {
+    // This will be called from subcompents to revalidate the current component
+    let final = false
+    if (currentSlide === content.length - 1) final = true
+    setNextDisable(final)
+  }
+
+  function _next() {
+    let next = currentSlide + 1
+    if (next == content.length) next = currentSlide // set disable
+    setCurrentSlide(next)
+  }
+
+  function _prev() {
+    let prev = currentSlide - 1
+    if (prev < 0) prev = 0 // set state to disable
+    setCurrentSlide(prev)
+  }
+  if (!loaded) return <Skeleton />
+  return (
     <div>
-        <h1 style={{textDecorationLine: 'underline'}}>{showTitle}</h1>
-        <br></br>
-        <div key={currentSlide} className={`animated ${showAnimation} faster`} style={{marginLeft: 'auto', marginRight: 'auto', justifyContent: 'center', display: 'flex'}}>
-          <PresentView
-            currentSlide={currentSlide}
-            high={highestSlide}
-            content={content[currentSlide]}
-            validate={validate}
-            finished={finished}
-            />
-        </div>
-        <Progress value={String(Math.round(((highestSlide+1)/content.length) * 100)) + '%'} />
-        <SlidesNav currentSlide={currentSlide} max={content.length} high={highestSlide} changeSlide={(slide) => setCurrentSlide(slide)}/>
-        <Navigation next={_next} prev={_prev} nextDisable={nextDisable} prevDisable={prevDisable}/>
-        <Finish visible={finished} />
+      <h2>{showTitle}</h2>
+      <br></br>
+      <div
+        key={currentSlide}
+        className={`animated ${showAnimation} faster`}
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          justifyContent: 'center',
+          display: 'flex',
+        }}>
+        <PresentView
+          currentSlide={currentSlide}
+          high={highestSlide}
+          content={content[currentSlide]}
+          validate={validate}
+          finished={finished}
+        />
+      </div>
+      <Progress
+        value={
+          String(Math.round(((highestSlide + 1) / content.length) * 100)) + '%'
+        }
+      />
+      <SlidesNav
+        currentSlide={currentSlide}
+        max={content.length}
+        high={highestSlide}
+        changeSlide={(slide) => setCurrentSlide(slide)}
+      />
+      <Navigation
+        next={_next}
+        prev={_prev}
+        nextDisable={nextDisable}
+        prevDisable={prevDisable}
+      />
+      <Finish visible={finished} />
     </div>
-    )
+  )
 }
-
-

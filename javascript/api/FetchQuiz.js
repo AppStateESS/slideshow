1ae01466823a5070d645d70676a5ec50b5df1894
Matt McNaney <mcnaneym@appstate.edu>
2021-07-22 11:28:02 -0400

    ES6 formatting

--- javascript/api/FetchQuiz.js
+++ javascript/api/FetchQuiz.js
@@ -1,27 +1,29 @@
-import "regenerator-runtime/runtime";
+import 'regenerator-runtime/runtime'
+
+/* global $ */
 const FetchQuiz = async function fetchQuiz(id) {
-    let quizContent = {}
-    await $.ajax({
-      url: './slideshow/Quiz/' + id,
-      type: 'GET',
-      dataType: 'json',
-      success: async (data) => {
-        // Convert array to object
-        data = data[0]
-        quizContent = {
-          question: data['question'],
-          answers: JSON.parse(data['answers']),
-          correct: JSON.parse(data['correct']),
-          type: data['type'],
-          feedback: JSON.parse(data['feedback']),
-          quizId: Number(data['id'])
-        }
-      },
-      error: (req, res) => {
-        console.error(res)
+  let quizContent = {}
+  await $.ajax({
+    url: './slideshow/Quiz/' + id,
+    type: 'GET',
+    dataType: 'json',
+    success: async (data) => {
+      // Convert array to object
+      data = data[0]
+      quizContent = {
+        question: data['question'],
+        answers: JSON.parse(data['answers']),
+        correct: JSON.parse(data['correct']),
+        type: data['type'],
+        feedback: JSON.parse(data['feedback']),
+        quizId: Number(data['id']),
       }
-    })
-    return quizContent
-  }
+    },
+    error: (req, res) => {
+      console.error(res)
+    },
+  })
+  return quizContent
+}
 
 export default FetchQuiz

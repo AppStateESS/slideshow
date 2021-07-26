e374cc5008a21b166dc6079fc01f4cea95fdf8ab
Matt McNaney <mcnaneym@appstate.edu>
2021-07-26 13:05:30 -0400

    Removing unused code.

--- javascript/AddOn/Empty.js
+++
@@ -1,3 +0,0 @@
-export default function empty(value) {
-  return (value === undefined || value === null || value === 0 || value === '0' || value.length === 0 || value === false)
-}
--- javascript/Slide/index.jsx
+++
@@ -1,6 +0,0 @@
-'use strict'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Slide from './Slide.jsx'
-
-ReactDOM.render(<Slide/>, document.getElementById('Slide'))

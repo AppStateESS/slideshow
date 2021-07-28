1c536a4047ff704bc685cbbb40aa34cb0cc183da
Matt McNaney <mcnaneym@appstate.edu>
2021-07-28 08:38:25 -0400

    Lint formatting.

--- javascript/Present/Viewspace.jsx
+++ javascript/Present/Viewspace.jsx
@@ -1,11 +1,11 @@
 'use strict'
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Editor, EditorState, convertFromRaw} from 'draft-js'
 
 import decorator from '../Resources/Draft/LinkDecorator.js'
 import CustomStyleMap from '../Resources/Draft/CustomStyleMap.js'
-import CustomBlockFn from '../Resources/Draft/CustomBlockFn.js';
+import CustomBlockFn from '../Resources/Draft/CustomBlockFn.js'
 
 import '../Edit/custom.css'
 
@@ -13,7 +13,7 @@ export default class Viewspace extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      editorState: EditorState.createEmpty(decorator)
+      editorState: EditorState.createEmpty(decorator),
     }
     this.loadEditorState = this.loadEditorState.bind(this)
   }
@@ -31,11 +31,13 @@ export default class Viewspace extends Component {
     }
   }
 
-  loadEditorState(content) {
-    let contentState = convertFromRaw(JSON.parse(this.props.content.saveContent))
+  loadEditorState() {
+    let contentState = convertFromRaw(
+      JSON.parse(this.props.content.saveContent)
+    )
 
     this.setState({
-      editorState: EditorState.createWithContent(contentState, decorator)
+      editorState: EditorState.createWithContent(contentState, decorator),
     })
   }
 
@@ -43,10 +45,15 @@ export default class Viewspace extends Component {
     let styles = CustomStyleMap
 
     if (this.state.editorState != undefined) {
-      // Custom text color 
-      const color = this.state.editorState.getCurrentInlineStyle().keys().next().value
+      // Custom text color
+      const color = this.state.editorState
+        .getCurrentInlineStyle()
+        .keys()
+        .next().value
       if (color != undefined) {
-        const styleObj = JSON.parse('{"' + color + '":{"color":"' + color + '"}}')
+        const styleObj = JSON.parse(
+          '{"' + color + '":{"color":"' + color + '"}}'
+        )
         styles = Object.assign(styleObj, CustomStyleMap)
       }
     }
@@ -55,20 +62,31 @@ export default class Viewspace extends Component {
     if (this.props.content.media != undefined) {
       image = (
         <div className="col">
-          <img src={this.props.content.media.imgUrl} alt={this.props.content.media} style={{height: '100%', width: '100%', objectFit: 'scale-down'}}></img>
+          <img
+            src={this.props.content.media.imgUrl}
+            alt={this.props.content.media}
+            style={{
+              height: '100%',
+              width: '100%',
+              objectFit: 'scale-down',
+            }}></img>
         </div>
       )
       align = this.props.content.media.align
     }
     return (
       <div className="row">
-        {(align === 'left') ? image : undefined}
+        {align === 'left' ? image : undefined}
         <div className="col">
-          <Editor editorState={this.state.editorState} customStyleMap={styles} blockStyleFn={CustomBlockFn} readOnly />
+          <Editor
+            editorState={this.state.editorState}
+            customStyleMap={styles}
+            blockStyleFn={CustomBlockFn}
+            readOnly
+          />
         </div>
-        {(align === 'right') ? image : undefined}
+        {align === 'right' ? image : undefined}
       </div>
-
     )
   }
 }
